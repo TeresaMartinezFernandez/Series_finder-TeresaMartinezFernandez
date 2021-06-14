9e7b52076d044bba3c4d5dc39b0d81540c5bf0f0
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-06-14 12:56:25 +0200

    updating links and remove btn series

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
-Evaluación final modulo 2 de Teresa Martinez 
+# Series browser
 
-Buscador de series
+The purpose of this project is to build a series browser, which can display a list with the result of the search and another list with the favourite series. I ran a fetch request to pull information from an API. By clicking on the Search button, the application must connect to the TVMaze open API for series search.This second list gets created once you click on each card of the main list, and the information is saved on local storage. In order to populate my data base.
 
-El ejercicio consiste en desarrollar una aplicación web de búsqueda de series de TV, que nos permite des/marcar las series como favoritas y guardarlas en local storage.
+The exercise is done with HTML, CSS AND JavaScript. To start the page from Visual Code open with Live Server.
 
-El ejercicio está hecho con HTML, CSS Y JavaScript. Para arrancar la página desde Visual Code abrir con Live Server.
+Some of the strings returned by the API do not have an image. In that case, a filling image is shown with the placeholder.com service where in the URL itself we indicate the size, colors, text: https://via.placeholder.com/210x295/ffffff/666666/? text = TV.
+Favorites: Once the search results appear, the user can indicate which are our favorite series. To do this, clicking on any series is shown in the list on the left of favorite series.
 
-La aplicación de búsqueda de series consta de dos partes:
-
-Un campo de texto y un botón para buscar series por su título.
-Un listado de resultados de búsqueda donde aparece el cartel de la serie y el título.
-Búsqueda:
-
-
-Al hacer clic sobre el botón de Buscar, la aplicación debe conectarse al API abierto de TVMaze para búsqueda de series.
-
-Algunas de las series que devuelve el API no tienen imagen. En ese caso se muestra una imagen de relleno con el servicio de placeholder.com donde en la propia URL indicamos el tamaño, colores, texto: https://via.placeholder.com/210x295/ffffff/666666/? text=TV.
-Favoritos: Una vez aparecen los resultados de búsqueda, la usuaria puede indicar cuáles son nuestras series favoritas. Para ello, al hacer clic sobre culaquier serie se muestra en el listado de la izquierda de series favoritas.
-
-Podemos borrar todo el listado de favoritas haciendo clic en el botón de borrar.
-
+We can delete the entire list of favorites by clicking the delete button.
--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let favoriteSeries = []; //Array que guarda los favoritos
 const urlPlaceholder =
   "https://via.placeholder.com/210x295/ffffff/666666/? text=TV.";
 
-//pido la lista de series al API
+//Get series from API and access the date we need
 
 function getDataFromApi() {
   const inputValue = inputElement.value;
@@ -48,7 +48,7 @@ function getFromLocalStorage() {
 //Arrancar la página
 getFromLocalStorage();
 
-//CAMPO DE BUSQUEDA
+//prevent the form from refreshing and sending data
 
 function handleSearchBtn(ev) {
   ev.preventDefault();
@@ -59,11 +59,13 @@ function handleSearchBtn(ev) {
 
 inputBtnElement.addEventListener("click", handleSearchBtn);
 
-//Pongo en la página las series buscadas
+//paint main series list
 
 function renderCards() {
   cardSeriesElement.innerHTML = "";
   let htmlCode = "";
+
+  // if it is in favourites, it adds a class to the li, so I can give it a different background colour; if it is not, it does not add it.
   for (const show of dataSeriesList) {
     let isFavoriteClass;
     if (isFavoriteSerie(show)) {
@@ -95,10 +97,15 @@ function renderCards() {
   listenSeriesClick();
 }
 
+// test if the series I receive on the parameter is in the favourite list by linking clicked "li" id in main series list to the "li" id in favourite series list
+
 function isFavoriteSerie(show) {
   const favoriteFound = favoriteSeries.find((favoriteSerie) => {
     return favoriteSerie.id === show.id;
   });
+
+  // if found with "find", it returns undefined.
+
   if (favoriteFound === undefined) {
     return false;
   } else {
@@ -106,35 +113,37 @@ function isFavoriteSerie(show) {
   }
 }
 
-//escucho click de favoritas
+//listen to the event on each card on the main series list
+
 function listenSeriesClick() {
   const listElementSeries = document.querySelectorAll(".js-list");
   for (const iterator of listElementSeries) {
     iterator.addEventListener("click", handleAddFavorites);
   }
 }
-// funcion que añade al array de favoritos la serie seleccionada y la quita
+//  check if the clicked card (id) on the main list is in favourites array or not. If it is not, push it, if it is, remove it.
+
 function handleAddFavorites(ev) {
   const clickedSerieId = parseInt(ev.currentTarget.id);
+  const serieFound = dataSeriesList.find(function (serie) {
+    return serie.id === clickedSerieId;
+  });
 
   const serieFoundIndex = favoriteSeries.findIndex(function (favorite) {
     return favorite.id === clickedSerieId;
   });
 
   if (serieFoundIndex === -1) {
-    const serieFound = dataSeriesList.find(function (serie) {
-      return serie.id === clickedSerieId;
-    });
-
     favoriteSeries.push(serieFound);
   } else {
     favoriteSeries.splice(serieFoundIndex, 1);
   }
   renderCards();
   renderFavoriteCards();
+  setInLocalStorage();
 }
 
-//funcion que renderiza las series favoritas
+// paint list with favourite movies
 
 function renderFavoriteCards() {
   const cardFavoriteElement = document.querySelector(".js-favorite__series");
@@ -142,7 +151,7 @@ function renderFavoriteCards() {
   let htmlCode = "";
   for (let favoriteSerie of favoriteSeries) {
     htmlCode += `<li class="js-list card__list--favorite" id="${favoriteSerie.id}">`;
-    htmlCode += `<h2 class="favorite__title--card js-card__title">${favoriteSerie.name}</h2><i class="far fa-times-circle js-buttonRemove" title="Eliminar serie de favoritos" aria-hidden="true"></i>`;
+    htmlCode += `<h2 class="favorite__title--card js-card__title">${favoriteSerie.name}</h2><i id="${favoriteSerie.id}"class="far fa-times-circle js-buttonRemove" title="Eliminar serie de favoritos" aria-hidden="true"></i>`;
 
     if (favoriteSerie.image === null) {
       htmlCode += `<img class="favorite-img"
@@ -158,6 +167,8 @@ function renderFavoriteCards() {
     }
     htmlCode += "</li>";
     cardFavoriteElement.innerHTML = htmlCode;
+
+    addListenersToDeleteButtons();
   }
 
   handleRunFavorites(ev);
@@ -168,34 +179,43 @@ function handleRunFavorites(ev) {
   renderFavoriteCards();
 }
 
-inputLogElement.addEventListener("click", handleRunFavorites);
-
-//FUNCION PARA ELIMINAR TODOS LOS FAVORITOS A LA VEZ
-const buttonRemoveAll = document.querySelector(".js-buttonRemoveAll");
-
-const removeAllFavorites = () => {
-  favoriteSeries = [];
-  renderFavoriteCards();
-};
-
-buttonRemoveAll.addEventListener("click", removeAllFavorites);
-
-//FUNCION PARA ELIMINAR INDIVIDUALMENTE CADA SERIE
-
-const removeFavorite = (ev) => {
-  const clickedButton = ev.currentTarget;
-  const clickedButtonParentId = parseInt(clickedButton.parentElement.id);
-  const favElementId = favoriteSeries.findIndex(
-    (favorite) => favorite.id === clickedButtonParentId
-  );
-  favoriteSeries.splice(favElementId, 1);
-  // saveFavoritesOnLocalStorage();
-  renderFavoriteCards();
-};
+// listen to individual remove buttons
 
 const addListenersToDeleteButtons = () => {
   const buttonsRemove = document.querySelectorAll(".js-buttonRemove");
+
   for (const button of buttonsRemove) {
     button.addEventListener("click", removeFavorite);
   }
 };
+
+// link clicked element id to id of favourite series array; then remove the clicked element
+
+const removeFavorite = (ev) => {
+  const clickedButton = parseInt(ev.currentTarget.id);
+
+  const favElementId = favoriteSeries.findIndex(
+    (series) => series.id === clickedButton
+  );
+  if (favElementId !== -1) {
+    favoriteSeries.splice(favElementId, 1);
+  }
+
+  renderFavoriteCards();
+  renderCards();
+};
+
+// inputLogElement.addEventListener("click", handleRunFavorites);
+
+// Remove all favorites series
+
+const buttonRemoveAll = document.querySelector(".js-buttonRemoveAll");
+
+const removeAllFavorites = () => {
+  localStorage.clear("favoriteSeries");
+  favoriteSeries = [];
+  renderFavoriteCards();
+  renderCards();
+};
+
+buttonRemoveAll.addEventListener("click", removeAllFavorites);
